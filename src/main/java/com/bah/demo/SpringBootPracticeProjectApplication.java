08e55f0a103fd16adb72be703a4be25fb31473e2
imcbee <imcbee@terpmail.umd.edu>
2023-03-23 15:04:51 -0400

    first commit

---
+++ src/main/java/com/bah/demo/SpringBootPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bah.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeProjectApplication.class, args);
+	}
+
+}
