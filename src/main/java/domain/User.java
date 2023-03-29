1dd05863bad1f5439103adbdbd7f3c7a865c4735
Caulfield <628777@bah.com>
2023-03-29 10:48:07 -0400

    added Lombok dependency and annotations to User model

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,10 +1,18 @@
 package domain;
 
 import domain.Products;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     private String username;
